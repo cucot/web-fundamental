3f3edd04ea166e1c71c731a5cd0efb7452b87aed
cucot <phucnn.work@gmail.com>
2021-02-06 14:38:23 +0700

    start learning some js

---
+++ javscript/script.js
@@ -0,0 +1,10 @@
+document.write('Script inside a div');
+function sayHello() {
+    alert('Hello, world');
+    // document.write('Hello, world');
+};
+function printHello() {
+    document.write('Hello, world');
+    // document.write('Hello, world');
+}
+document.getElementById("js").innerHTML="changed in script.js";
---
+++ javscript/variable/script.js
@@ -0,0 +1,25 @@
+var a = 10;
+//a = 'global'
+var b = 15;
+var myvar = 'abc'
+var yourvar= 'bc'
+var c, d;//these 2 values are known as undefined variables
+var e = 16, f = 20;
+function printSum() {
+    //var a = 'local';
+    console.log('a + b + c +  d + e + f = ' + a+b+myvar+c+d+e+f);
+    console.log(4+5);
+    console.log(a-b);
+    console.log
+}
+/*
+a + b = 43
+a-b=23
+a / b = 3.3
+a % b = 3
+a + b + c = 43Test
+++a = 35
+--b = 8
+
+
+*/
